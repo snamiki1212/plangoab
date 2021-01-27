ebfb5bfb3de78dd2a6582394935b9aff535e9312
snamiki1212 <snamiki1212@gmail.com>
2021-01-26 23:52:41 -0800

    Refactor logics

--- src/components/organisms/FullCalendar.tsx
+++ src/components/organisms/FullCalendar.tsx
@@ -1,22 +1,16 @@
 import React from "react";
-import FullCalendarLib, {
-  DateSelectArg,
-  EventClickArg,
-  EventInput,
-} from "@fullcalendar/react";
+import FullCalendarLib from "@fullcalendar/react";
 import resourceTimelinePlugin from "@fullcalendar/resource-timeline";
 import listPlugin from "@fullcalendar/list";
 import interactionPlugin from "@fullcalendar/interaction";
-import { uuid } from "../../lib/uuid";
 import { useAgeEvents } from "../../hooks/useAgeEvents";
 import { useStoryList } from "../../hooks/useStoryList";
 import { useAgeContext } from "../../hooks/useAgeContext";
+import { useEventsHandler } from "../../hooks/useEventsHandler";
 import { FULL_CALENDAR_CONFIGS } from "../../constants/fullcalendar";
 import { SHARED__RESOURCES } from "../../constants/fullcalendar";
 
 export const FullCalendar = () => {
-  const [_events, setEvents] = React.useState<EventInput[]>([]);
-
   const { birth } = useAgeContext();
   const [ageEvents, calcAgeEvents] = useAgeEvents();
   const {
@@ -34,42 +28,27 @@ export const FullCalendar = () => {
     [storyResources]
   );
 
+  const _events = React.useMemo(() => [...ageEvents, ...storyEvents], [
+    ageEvents,
+    storyEvents,
+  ]);
+
+  const { events, select, click, set: setEvents } = useEventsHandler();
+
+  React.useEffect(() => {
+    setEvents(_events)
+  }, [_events, setEvents])
+
   React.useEffect(() => {
     calcAgeEvents(birth);
   }, [birth, calcAgeEvents]);
-
-  React.useEffect(() => {
-    setEvents([...ageEvents, ...storyEvents]);
-  }, [ageEvents, storyEvents]);
-
-  const select = (info: DateSelectArg) => {
-    setEvents((prev) => {
-      return [
-        ...prev,
-        {
-          id: uuid(),
-          resourceId: info.resource?.id,
-          start: info.startStr,
-          end: info.endStr,
-        },
-      ];
-    });
-  };
-
-  const click = (info: EventClickArg) => {
-    if (!window.confirm("Would you like to remove this event?")) return;
-    const id = info.event.id;
-    setEvents((prev) => {
-      return prev.filter((e) => e.id !== id);
-    });
-  };
 
   return (
     <FullCalendarLib
       selectable={true}
       editable={true}
       plugins={[interactionPlugin, resourceTimelinePlugin, listPlugin]}
-      events={_events}
+      events={events}
       resources={_resources}
       select={select}
       eventClick={click}
