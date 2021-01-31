d326fd0c68c39f8dc76ca7e6fa1abf5e577873f6
snamiki1212 <snamiki1212@gmail.com>
2021-01-31 11:08:49 -0800

    Update page laytout

--- src/components/pages/CalendarPage.tsx
+++ src/components/pages/CalendarPage.tsx
@@ -5,8 +5,10 @@ import { FullCalendar } from "../organisms/FullCalendar";
 export const CalendarPage: React.VFC = () => {
   return (
     <div>
+      <h2>Your Profile Data</h2>
       <ProfileCard />
-      <hr />
+
+      <h2>Calendar</h2>
       <FullCalendar />
     </div>
   );
