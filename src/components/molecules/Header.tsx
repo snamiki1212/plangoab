a6860a8533eba2c334db5fb22460827492fe0a06
snamiki1212 <snamiki1212@gmail.com>
2021-02-11 00:08:25 -0800

    Add about explaination

--- src/components/molecules/Header.tsx
+++ src/components/molecules/Header.tsx
@@ -1,11 +1,30 @@
 import React from "react";
+import Button from "@material-ui/core/Button";
 import styled from "styled-components";
+import { AboutModal } from "../atoms/AboutModal";
 
 export function Header() {
+  const [isOpen, setIsOpen] = React.useState(false);
+
+  const handleClick = () => {
+    console.log("handle click");
+    setIsOpen(true);
+  };
+
+  const handleClose = () => {
+    setIsOpen(false);
+  };
+
   return (
-    <Container>
-      <Title>✈️Plangoab</Title>
-    </Container>
+    <>
+      <Container>
+        <Title>Plangoab</Title>
+        <Button onClick={handleClick}>About Plangoab</Button>
+      </Container>
+      <AboutModal isOpen={isOpen} onClose={handleClose} />
+
+      {/*  */}
+    </>
   );
 }
 
---
+++ src/components/pages/HowToUsePage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const text = `this is message`;
+
+export function HowToUsePage() {
+  return <div>{text}</div>;
+}
