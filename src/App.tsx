aa04bba16c364833d03b116f512940c53f6ccddc
snamiki1212 <snamiki1212@gmail.com>
2021-02-10 23:16:47 -0800

    Add reset.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { MuiPickersUtilsProvider } from "./providers/MuiPickersUtilsProvider";
 import { CalendarPage } from "./components/pages/CalendarPage";
+import './reset.css';
 
 function App() {
   return (
