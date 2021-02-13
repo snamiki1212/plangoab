92d616e049417ca5bd78a91ab087e16e4b4537a9
snamiki1212 <snamiki1212@gmail.com>
2021-02-13 15:53:28 -0800

    Fix footer component

--- src/components/molecules/Footer.tsx
+++ src/components/molecules/Footer.tsx
@@ -1,29 +1,41 @@
 import React from "react";
 import styled from "styled-components";
+import { LogoImage } from "../atoms/LogoImage";
+
+const THIS_GITHUB_URL =
+  "https://github.com/snamiki1212/canada-immigration-scheduler";
 
 export function Footer() {
   return (
-    <Wrapper>
-      <LogoText>✈️Plangoab</LogoText>
-      <a
-        href="https://github.com/snamiki1212/canada-immigration-scheduler"
-        target="_blank"
-        rel="noreferrer"
-      >
+    <Container>
+      <LogoText>
+        <LogoImageContainer>
+
+        <LogoImage />
+        </LogoImageContainer>
+        <span>Plangoab</span>
+      </LogoText>
+      <a href={THIS_GITHUB_URL} target="_blank" rel="noreferrer">
         Github
       </a>
-    </Wrapper>
+    </Container>
   );
 }
 
-const Wrapper = styled.div`
+const Container = styled.div`
   padding: 5rem;
   min-height: 30vh;
   background: var(--main-color);
   color: white;
 `;
 
+const LogoImageContainer = styled.div`
+  height: 4rem;
+  width: 4rem;
+`
+
 const LogoText = styled.div`
+  display: flex;
   color: white;
   font-size: 4rem;
-` 
+`;
