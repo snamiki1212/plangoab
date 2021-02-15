daa282454a0fe704a984416566ac4cd41b14638d
snamiki1212 <snamiki1212@gmail.com>
2021-02-14 18:24:07 -0800

    Refactor konnmari

--- src/components/molecules/ResourceModal.tsx
+++ src/components/molecules/ResourceModal.tsx
@@ -2,27 +2,15 @@ import React from "react";
 import Dialog from "@material-ui/core/Dialog";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import DialogContent from "@material-ui/core/DialogContent";
-import Typography from "@material-ui/core/Typography";
 //
 import { useForm } from "react-hook-form";
 //
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import {
-  pushAction,
   selectResourceModal,
   selectResource,
 } from "../../redux/ui/resourceModal";
-import {
-  removeResourceAction,
-  updateResourceAction,
-} from "../../redux/features/userCalendars";
-import { MY_CALENDAR_ID } from "../../constants/fullcalendar/settings";
-import { BaseResource, updateResource } from "../../core/resource/BaseResource";
-import {
-  NAME_OF_ORDER,
-  NAME_OF_STORY_ID,
-  FIELD_NAME,
-} from "../../constants/fullcalendar/settings";
+import { FIELD_NAME } from "../../constants/fullcalendar/settings";
 import { useResource } from "../../hooks/useResource";
 
 const H1 = FIELD_NAME["H1"];
@@ -36,8 +24,8 @@ type Props = {
 export function ResourceModal({ isOpen, onClose }: Props) {
   const resourceModal = useSelector(selectResourceModal);
   const resource = useSelector(selectResource);
-  const h1 = (resource && resource[H1]) ?? ""
-  const h2 = (resource && resource[H2]) ?? ""
+  const h1 = (resource && resource[H1]) ?? "";
+  const h2 = (resource && resource[H2]) ?? "";
 
   const { remove, update } = useResource();
 
@@ -60,30 +48,20 @@ export function ResourceModal({ isOpen, onClose }: Props) {
     [resourceModal, update, onClose]
   );
 
+  // TODO: error handler
   const { register, handleSubmit, errors } = useForm();
 
   return (
     <Dialog onClose={onClose} open={isOpen}>
       <DialogTitle>Resource</DialogTitle>
       <DialogContent dividers={true}>
-        <Typography>this is content</Typography>
-        <Typography>this is content</Typography>
-        <div>{resourceModal?.storyId}</div>
-        <button onClick={onRemove}>resource:remove</button>
-        {/*  */}
         <form onSubmit={handleSubmit(onUpdate)}>
-          <input
-            ref={register}
-            name={FIELD_NAME["H1"]}
-            defaultValue={h1}
-          />
-          <input
-            ref={register}
-            name={FIELD_NAME["H2"]}
-            defaultValue={h2}
-          />
-          <input type="submit" value="update" />
+          <input ref={register} name={FIELD_NAME["H1"]} defaultValue={h1} />
+          <input ref={register} name={FIELD_NAME["H2"]} defaultValue={h2} />
+          <input type="submit" value="Update" />
         </form>
+        <hr />
+        <button onClick={onRemove}>Remove</button>
       </DialogContent>
     </Dialog>
   );
