fa9935ce5aaec00551fb4d55ec6c6e6412e74549
snamiki1212 <snamiki1212@gmail.com>
2021-02-15 11:50:53 -0800

    Update template to become modal

--- src/components/organisms/CalendarList.tsx
+++ src/components/organisms/CalendarList.tsx
@@ -1,22 +1,34 @@
 import React from "react";
 import styled from "styled-components";
+import Dialog from "@material-ui/core/Dialog";
 import { UserCalendarContainer } from "../molecules/UserCalendarContainer";
 import { TemplateCalendarContainer } from "../molecules/TemplateCalendarContainer";
+import { useModal } from "../../hooks/useModal";
 
 export function CalendarList() {
-  return (
-    <Container>
-      <div>
-        <h2>User Calendar</h2>
-        <UserCalendarContainer />
-      </div>
+  const {
+    open: openCommunityCollege,
+    close: closeCommunityCollege,
+    isOpen: isOpenCommunityCollege,
+  } = useModal();
 
-      <hr />
-      <div>
-        <h2>Community College</h2>
+  return (
+    <>
+      <Container>
+        <button onClick={openCommunityCollege}>Community College</button>
+        <hr />
+        <div>
+          <h2>User Calendar</h2>
+          <UserCalendarContainer />
+        </div>
+      </Container>
+      
+      {/* Modal */}
+      <Dialog fullScreen open={isOpenCommunityCollege} onClose={closeCommunityCollege}>
+        <button onClick={closeCommunityCollege}>Close</button>
         <TemplateCalendarContainer />
-      </div>
-    </Container>
+      </Dialog>
+    </>
   );
 }
 
