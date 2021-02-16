eac51e9f268ed6048ea786d3de14b80f32ead257
snamiki1212 <snamiki1212@gmail.com>
2021-02-15 16:41:54 -0800

    Fix ts check

--- src/hooks/useResourceGroupLabelContentInTemplateCalendar.tsx
+++ src/hooks/useResourceGroupLabelContentInTemplateCalendar.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { selectCommunityCollegeCalendar } from "../redux/features/templateCalendarTable";
 import { MY_CALENDAR_ID } from "../constants/fullcalendar/settings";
 
@@ -15,7 +15,6 @@ type Props = {
 export const useResourceGroupLabelContentInTemplateCalendar = ({
   createClickHandel,
 }: Props) => {
-  const dispatch = useDispatch();
   const calendar = useSelector(selectCommunityCollegeCalendar);
 
   const resourceGroupLabelContent = React.useCallback(
--- src/hooks/useStory.tsx
+++ src/hooks/useStory.tsx
@@ -1,32 +1,32 @@
 import React from "react";
 import { useDispatch } from "react-redux";
-import { removeStoryAction, updateStoryAction } from "../redux/features/userCalendars";
-import { BaseStory,updateStory } from "../core/story/BaseStory";
+import {
+  removeStoryAction,
+  updateStoryAction,
+} from "../redux/features/userCalendars";
+import { BaseStory, updateStory } from "../core/story/BaseStory";
 
 type IdSet = { calendarId: string; storyId: string };
 
 export const useStory = () => {
   const dispatch = useDispatch();
 
-  const remove = React.useCallback(
-    ({ calendarId, storyId }: IdSet) => {
-      if (!window.confirm("Do you remove this story?")) return;
-      dispatch(
-        removeStoryAction({
-          storyId,
-          calendarId,
-        })
-      );
-    },
-    [dispatch]
-  );
+  const remove = React.useCallback(({ calendarId, storyId }: IdSet) => {
+    if (!window.confirm("Do you remove this story?")) return;
+    dispatch(
+      removeStoryAction({
+        storyId,
+        calendarId,
+      })
+    );
+  }, [dispatch]);
 
   const update = React.useCallback(
-    (idSet:IdSet, story: BaseStory, data: any) => {
+    (idSet: IdSet, story: BaseStory, data: any) => {
       const newStory = updateStory(story, data);
       dispatch(updateStoryAction({ ...idSet, newStory }));
     },
-    [updateStory, updateStoryAction, dispatch]
+    [dispatch]
   );
 
   return { remove, update } as const;
