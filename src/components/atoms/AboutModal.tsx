441e3e6f753542d5e4d0a3afaf711c0522403a4a
snamiki1212 <snamiki1212@gmail.com>
2021-02-16 21:15:04 -0800

    Update about page

--- src/components/atoms/AboutModal.tsx
+++ src/components/atoms/AboutModal.tsx
@@ -1,8 +1,9 @@
 import React from "react";
+import styled from "styled-components";
 import Dialog from "@material-ui/core/Dialog";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import DialogContent from "@material-ui/core/DialogContent";
-import Typography from "@material-ui/core/Typography";
+import * as AuthorInfo from "../../constants/meta";
 
 type Props = {
   isOpen: boolean;
@@ -13,10 +14,46 @@ export function AboutModal({ isOpen, onClose }: Props) {
   return (
     <Dialog onClose={onClose} open={isOpen}>
       <DialogTitle>About Plangoab</DialogTitle>
-      <DialogContent dividers={true}>
-        <Typography>this is content</Typography>
-        <Typography>this is content</Typography>
-      </DialogContent>
+      <_DialogContent dividers={true}>
+        <div>
+          <h2>About</h2>
+          <p>
+            A web calendar for a person going abroad, generating a
+            suitable schedule and customizable. Using React, Redux(Redux
+            Toolkit), and Fullcalendar.
+          </p>
+        </div>
+        <div>
+          <h2>License</h2>
+          <li>GPL-3.0</li>
+        </div>
+
+        <div>
+          <h2>Author</h2>
+          <li>
+            <a href={AuthorInfo.MY_GITHUB_URL}>GitHub:snamiki1212</a>
+          </li>
+          <li>
+            <a href={AuthorInfo.MY_TWITTER_URL}>Twitter:snamiki1212</a>
+          </li>
+          <li>
+            <a href={AuthorInfo.MY_PORTFOLIO}>Portfolio</a>
+          </li>
+        </div>
+
+        <div>
+          <h2>Source</h2>
+          <span>
+            This page is managed at{" "}
+            <a href={AuthorInfo.THIS_GITHUB_URL}>GitHub</a>.
+          </span>
+        </div>
+      </_DialogContent>
     </Dialog>
   );
 }
+
+const _DialogContent = styled(DialogContent)`
+  display: grid;
+  gap: 2rem;
+`;
---
+++ src/constants/meta.ts
@@ -0,0 +1,4 @@
+export const THIS_GITHUB_URL = "https://github.com/snamiki1212/plangoab";
+export const MY_GITHUB_URL = "https://github.com/snamiki1212";
+export const MY_TWITTER_URL = "https://twitter.com/snamiki1212";
+export const MY_PORTFOLIO = "https://snamiki1212.github.io";
