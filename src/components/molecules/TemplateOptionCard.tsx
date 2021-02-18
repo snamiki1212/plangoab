ed3b715b9ff7181df2ee848addc5b96ade4d02e7
snamiki1212 <snamiki1212@gmail.com>
2021-02-17 18:44:32 -0800

    Update template option controller card

--- src/components/molecules/TemplateOptionCard.tsx
+++ src/components/molecules/TemplateOptionCard.tsx
@@ -1,35 +1,45 @@
 import React from "react";
 import styled from "styled-components";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
 
 const MAX_PERIOD_MONTH = 12 * 5;
 
 export function TemplateOptionCard() {
   return (
-    <Container>
-      <h2>Template Options(TODO: not working yet)</h2>
-      <div>
-        <label>School Period(Month)</label>
-        <input type="range" min="0" max={MAX_PERIOD_MONTH} id="schoolPeriod" />
-      </div>
-      <div>
-        <label>Co-op Period(Month)</label>
-        <input type="range" min="0" max={MAX_PERIOD_MONTH} id="schoolPeriod" />
-      </div>
-      <div>
-        <span>Start From (Month)</span>
-        <span>
-          {Array.from({ length: 12 }).map((_, idx) => {
-            const month = idx + 1;
-            return (
-              <CheckboxListItem>
-                <label>{month}</label>
-                <input type="checkbox" value={month} />
-              </CheckboxListItem>
-            );
-          })}
-        </span>
-      </div>
-    </Container>
+    <Card>
+      <CardContent>
+        <h2>Template Options(TODO: not working yet)</h2>
+        <InnerContainer>
+          <SchoolPeriodLabel>School Period(Month)</SchoolPeriodLabel>
+          <SchoolPeriodInput
+            type="range"
+            min="0"
+            max={MAX_PERIOD_MONTH}
+            id="schoolPeriod"
+          />
+          <CoopPeriodLabel>Co-op Period(Month)</CoopPeriodLabel>
+          <CoopPeriodInput
+            type="range"
+            min="0"
+            max={MAX_PERIOD_MONTH}
+            id="schoolPeriod"
+          />
+          <StartSchoolFrom>Start From (Month)</StartSchoolFrom>
+          <StartSchoolOptions>
+            {Array.from({ length: 12 }).map((_, idx) => {
+              const month = idx + 1;
+              return (
+                <CheckboxListItem>
+                  <label>{month}</label>
+                  <input type="checkbox" value={month} />
+                </CheckboxListItem>
+              );
+            })}
+          </StartSchoolOptions>
+        </InnerContainer>
+      </CardContent>
+    </Card>
   );
 }
 
@@ -38,4 +48,36 @@ const CheckboxListItem = styled.div`
   margin-right: 0.5rem;
 `;
 
-const Container = styled.div``;
+const InnerContainer = styled.div`
+  display: grid;
+  gap: 1rem;
+  padding: 0.5rem;
+  grid-template-areas:
+    "schoolPeriodLabel schoolPeriodInput"
+    "coopPeriodLabel coopPeriodInput"
+    "startSchoolFrom startSchoolOptions";
+`;
+
+const SchoolPeriodLabel = styled.label`
+  grid-area: schoolPeriodLabel;
+`;
+
+const SchoolPeriodInput = styled.input`
+  grid-area: schoolPeriodInput;
+`;
+
+const CoopPeriodLabel = styled.label`
+  grid-area: coopPeriodLabel;
+`;
+
+const CoopPeriodInput = styled.input`
+  grid-area: coopPeriodInput;
+`;
+
+const StartSchoolFrom = styled.div`
+  grid-area: startSchoolFrom;
+`;
+
+const StartSchoolOptions = styled.div`
+  grid-area: startSchoolOptions;
+`;
