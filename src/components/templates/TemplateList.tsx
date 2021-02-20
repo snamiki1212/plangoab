2b6d50fc27ab08851559095023e13ac1052cffe2
snamiki1212 <snamiki1212@gmail.com>
2021-02-20 11:23:06 -0800

    Remove esl college case because of bein covered by private collage case

--- src/components/templates/TemplateList.tsx
+++ src/components/templates/TemplateList.tsx
@@ -5,7 +5,6 @@ import Box from "@material-ui/core/Box";
 
 import { PrivateCollegeTemplate } from "./PrivateCollegeTemplate";
 import { PublicCollegeTemplate } from "./PublicCollegeTemplate";
-import { EslTemplate } from "./EslTemplate";
 
 const TabPanel = ({
   shouldShow,
@@ -35,16 +34,12 @@ export function TemplateList() {
       <Tabs value={selectedIdx} onChange={handleChange}>
         <Tab label="Private College" />
         <Tab label="Public College" />
-        <Tab label="ESL" />
       </Tabs>
       <TabPanel shouldShow={selectedIdx === 0}>
         <PrivateCollegeTemplate />
       </TabPanel>
       <TabPanel shouldShow={selectedIdx === 1}>
         <PublicCollegeTemplate />
-      </TabPanel>
-      <TabPanel shouldShow={selectedIdx === 2}>
-        <EslTemplate />
       </TabPanel>
     </div>
   );
