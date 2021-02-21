5e5653375a429d8c4dc1d1a39106264729a62e50
snamiki1212 <snamiki1212@gmail.com>
2021-02-21 09:33:03 -0800

    Update loading animation img

--- src/components/pages/LoadingPage.tsx
+++ src/components/pages/LoadingPage.tsx
@@ -1,5 +1,21 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+import CircularProgress from "@material-ui/core/CircularProgress";
 
-export function LoadingPage (){
-  return(<div>loading...</div>)
+export function LoadingPage() {
+  return (
+    <Container>
+        <div>
+          <CircularProgress />
+        </div>
+        <span>- Loading -</span>
+    </Container>
+  );
 }
+
+const Container = styled.div`
+  height: 100vh;
+  display: grid;
+  place-items: center;
+  align-content: center;
+`;
