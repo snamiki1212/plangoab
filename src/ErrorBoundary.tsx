f32d96d9e11a846b1e618962c61134cf8a6b94d1
snamiki1212 <snamiki1212@gmail.com>
2021-02-22 17:20:01 -0800

    Add error boundary component

---
+++ src/ErrorBoundary.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { ErrorPage } from "./components/pages/ErrorPage";
+
+export class ErrorBoundary extends React.Component<any, { hasError: boolean }> {
+  constructor(props: any) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error: any) {
+    console.error(error);
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: any, errorInfo: any) {
+    console.error(error);
+    console.error(errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <ErrorPage />;
+    }
+    return this.props.children;
+  }
+}
