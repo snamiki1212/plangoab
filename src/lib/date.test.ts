e02d8325017bd4c5e511b2fa9a416f9dbf0ec601
snamiki1212 <snamiki1212@gmail.com>
2021-02-25 17:17:19 -0800

    Fix date logic

--- src/lib/date.test.ts
+++ src/lib/date.test.ts
@@ -1,15 +1,16 @@
 import { convertIsoToDateTime, convertIsoToYearAndMonth } from "./date";
 
-const dummyDate = new Date("2020-01-01 12:23:34");
+const dummyDate = new Date("2020-06-01 12:23:34 GMT");
 
 describe("func: convertIsoToDateTime", () => {
   it("sucess", () => {
-    expect(convertIsoToDateTime(dummyDate)).toBe("2020-01-01");
+    expect(convertIsoToDateTime(dummyDate)).toBe("2020-06-01");
   });
 });
 
 describe("func: convertIsoToYearAndMonth", () => {
   it("sucess", () => {
-    expect(convertIsoToYearAndMonth(dummyDate)).toBe("2020-01");
+    expect(convertIsoToYearAndMonth(dummyDate)).toBe("2020-06");
+    expect(convertIsoToYearAndMonth(dummyDate.toISOString())).toBe("2020-06");
   });
 });
