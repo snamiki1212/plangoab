0674e08d447032a3753df9415f3b4a14fa0eb340
snamiki1212 <snamiki1212@gmail.com>
2021-03-07 18:00:20 -0800

    Add calc age test

--- src/lib/age.test.ts
+++ src/lib/age.test.ts
@@ -1,4 +1,4 @@
-import { getRangeNumbers } from "./age";
+import { getRangeNumbers, calcAge } from "./age";
 
 describe("func: getRangeNumbers", () => {
   it("common", () => {
@@ -15,5 +15,39 @@ describe("func: getRangeNumbers", () => {
     const result = getRangeNumbers(2, 1);
     const expected = [] as number[];
     expect(result).toEqual(expected);
+  });
+});
+
+describe("func: calcAge", () => {
+  const dummyToday = "2020-12-13";
+
+  beforeAll(() => {
+    jest.useFakeTimers("modern"); // tell Jest to use a different timer implementation.
+    jest.setSystemTime(new Date(dummyToday).getTime());
+  });
+
+  afterAll(() => {
+    jest.useRealTimers();
+  });
+
+  it("general", () => {
+    const birth0 = new Date("1980-12-12");
+    const birth1 = new Date("1990-12-12");
+    const birth2 = new Date("2000-12-12");
+    const birth3 = new Date("2010-12-12");
+
+    expect(calcAge(birth0)).toEqual(40);
+    expect(calcAge(birth1)).toEqual(30);
+    expect(calcAge(birth2)).toEqual(20);
+    expect(calcAge(birth3)).toEqual(10);
+  });
+
+  it("1day threshold", () => {
+    const prevBirth = new Date("1990-12-12");
+    const sameBirth = new Date("1990-12-13");
+    const nextBirth = new Date("1990-12-14");
+    expect(calcAge(prevBirth)).toEqual(30);
+    expect(calcAge(sameBirth)).toEqual(30);
+    expect(calcAge(nextBirth)).toEqual(29);
   });
 });
