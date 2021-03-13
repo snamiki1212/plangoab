343a5e6945c9343e4e84603baa47d49d09c75ba2
snamiki1212 <snamiki1212@gmail.com>
2021-03-13 12:24:15 -0800

    Fix ts compile error

--- src/components/organisms/AdvancedOptions.tsx
+++ src/components/organisms/AdvancedOptions.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import styled from "styled-components";
 import TextField from "@material-ui/core/TextField";
-import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import InputAdornment from "@material-ui/core/InputAdornment";
 import { useTemplateOptions } from "../../hooks/useTemplateOptions";
