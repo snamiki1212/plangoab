2b0b7e3233c1d5858f4c0a8b75746100c2878733
snamiki1212 <snamiki1212@gmail.com>
2021-04-09 21:57:39 -0700

    Add collabrations section

--- src/components/molecules/Footer.tsx
+++ src/components/molecules/Footer.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import { LogoImage } from "../atoms/LogoImage";
+import { CollaborationList } from "./CollaborationList";
 
 export function Footer() {
   return (
@@ -9,11 +10,21 @@ export function Footer() {
         <LogoImage />
         <AppName>Plangoab</AppName>
       </Box>
+
+      <Box>
+        <div>
+          <h3>Collcations</h3>
+          <hr />
+          <CollaborationList />
+        </div>
+      </Box>
     </Container>
   );
 }
 
 const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
   padding: 5rem;
   border: 1px solid lightgray;
   box-shadow: 0 -4px 4px lightgray;
--- src/constants/meta.ts
+++ src/constants/meta.ts
@@ -1,3 +1,4 @@
+// TODO: rename to meta.ts to sns.ts
 export const THIS_GITHUB_URL = "https://github.com/snamiki1212/plangoab";
 export const MY_GITHUB_URL = "https://github.com/snamiki1212";
 export const MY_TWITTER_URL = "https://twitter.com/snamiki1212";
