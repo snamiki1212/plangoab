cbd775adf5d5445207b9e025e51978f5bcdbc0e4
snamiki1212 <snamiki1212@gmail.com>
2021-05-06 19:28:31 -0700

    Fix component name explicitily

--- plangoab/pages/index.tsx
+++ plangoab/pages/index.tsx
@@ -1,21 +1,23 @@
-import React from 'react'
-import FullCalendar from '@fullcalendar/react'
-import interactionPlugin from '@fullcalendar/interaction'
-import resourceTimelinePlugin from '@fullcalendar/resource-timeline'
+import React from "react";
+import FullCalendar from "@fullcalendar/react";
+import interactionPlugin from "@fullcalendar/interaction";
+import resourceTimelinePlugin from "@fullcalendar/resource-timeline";
 
-export default () => (
+const Index = () => (
   <FullCalendar
     plugins={[interactionPlugin, resourceTimelinePlugin]}
-    initialView='resourceTimelineDay'
+    initialView="resourceTimelineDay"
     nowIndicator={true}
     editable={true}
     initialEvents={[
-      { title: 'nice event', start: new Date(), resourceId: 'a' }
+      { title: "nice event", start: new Date(), resourceId: "a" },
     ]}
     initialResources={[
-      { id: 'a', title: 'Auditorium A' },
-      { id: 'b', title: 'Auditorium B' },
-      { id: 'c', title: 'Auditorium C' }
+      { id: "a", title: "Auditorium A" },
+      { id: "b", title: "Auditorium B" },
+      { id: "c", title: "Auditorium C" },
     ]}
   />
-)
+);
+
+export default Index;
