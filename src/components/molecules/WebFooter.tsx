efe5c7b14c11282ab275c99215e2c1d6e42acb52
snamiki1212 <snamiki1212@gmail.com>
2021-05-11 15:50:24 -0700

    Update footer/header UI

--- src/components/molecules/WebFooter.tsx
+++ src/components/molecules/WebFooter.tsx
@@ -1,3 +1,11 @@
+import Container from "@material-ui/core/Container";
+import styled from "styled-components";
+
 export function WebFooter() {
-  return <div>this is footer</div>;
+  return <_Container>this is footer</_Container>;
 }
+
+const _Container = styled(Container)`
+  min-height: 15rem;
+  background: lightblue;
+`;
--- src/components/molecules/WebHeader.tsx
+++ src/components/molecules/WebHeader.tsx
@@ -2,20 +2,35 @@ import Link from "next/link";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import { LogoImage } from "@/components/atoms/LogoImage";
+import styled from "styled-components";
 
 export function WebHeader() {
   return (
     <AppBar position="static">
       <Toolbar>
         <Link href="/home">
-          <a>
-            <div style={{ width: "50px", height: "50px" }}>
-              <LogoImage />
-            </div>
-            <span>Plangoab</span>
+          <a style={{ textDecoration: "none" }}>
+            <LogoContainer>
+              <div style={{ width: "50px", height: "50px" }}>
+                <LogoImage />
+              </div>
+              <LogoText>Plangoab</LogoText>
+            </LogoContainer>
           </a>
         </Link>
       </Toolbar>
     </AppBar>
   );
 }
+
+const LogoContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 0.5rem;
+`;
+
+const LogoText = styled.span`
+  font-weight: bold;
+  color: white;
+`;
