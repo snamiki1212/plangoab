8a462b5200829e16fc107490d9da87b7685f0ad3
snamiki1212 <snamiki1212@gmail.com>
2021-05-12 23:52:09 -0700

    Remove not using pkg

--- src/components/atoms/CalendarBase.tsx
+++ src/components/atoms/CalendarBase.tsx
@@ -1,7 +1,6 @@
 import React, { PropsWithChildren } from "react";
 import FullCalendarLib from "@fullcalendar/react";
 import resourceTimelinePlugin from "@fullcalendar/resource-timeline";
-import listPlugin from "@fullcalendar/list";
 import interactionPlugin from "@fullcalendar/interaction";
 import { FULL_CALENDAR_CONFIGS } from "@/constants/fullcalendar/options";
 
@@ -9,7 +8,7 @@ export function CalendarBase<T>(props: PropsWithChildren<T>) {
   return (
     <FullCalendarLib
       {...FULL_CALENDAR_CONFIGS}
-      plugins={[interactionPlugin, resourceTimelinePlugin, listPlugin]}
+      plugins={[interactionPlugin, resourceTimelinePlugin]}
       {...props}
     />
   );
