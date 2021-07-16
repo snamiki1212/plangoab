65b3b93aa8cd294897b0fea284c0f5d042930e26
snamiki1212 <snamiki1212@gmail.com>
2021-07-15 22:02:42 -0700

    Add story-modal test

---
+++ src/redux/ui/storyModal.test.ts
@@ -0,0 +1,66 @@
+import reducer, {
+  selectIsOpen,
+  selectStory,
+  selectStoryModal,
+  pushAction,
+  popAction,
+} from "./storyModal";
+import { RootState } from "../rootReducer";
+
+const createDummyStory = () => ({
+  calendarId: "calendarId",
+  storyId: "storyId",
+});
+
+const createRootState = (partialState: any) =>
+  ({ ui: { storyModal: { story: partialState } } } as RootState);
+
+describe(reducer.name, () => {
+  it("can init.", () => {
+    expect(reducer(undefined, {} as any)).toEqual({ story: null });
+  });
+
+  describe(pushAction.name, () => {
+    it("can work.", () => {
+      const story = createDummyStory();
+      const befState = { story: null };
+      const aftState = { story };
+      expect(reducer(befState, pushAction(story))).toEqual(aftState);
+    });
+  });
+
+  describe(popAction.name, () => {
+    it("can work.", () => {
+      const story = createDummyStory();
+      const befState = { story };
+      const aftState = { story: null };
+      expect(reducer(befState, popAction())).toEqual(aftState);
+    });
+  });
+});
+
+describe(selectIsOpen.name, () => {
+  it("should be true when to open.", () => {
+    const story = createDummyStory();
+    const rootState = createRootState(story);
+    expect(selectIsOpen(rootState)).toEqual(true);
+  });
+  it("should be false when to close.", () => {
+    const rootState = createRootState(null);
+    expect(selectIsOpen(rootState)).toEqual(false);
+  });
+});
+
+describe(selectStory.name, () => {
+  it.skip("can select.", () => {
+    // TODO: Fix logic using re-select feature in RTK because for now it's not testable.
+  });
+});
+
+describe(selectStoryModal.name, () => {
+  it("can select.", () => {
+    const story = createDummyStory();
+    const rootState = createRootState(story);
+    expect(selectStoryModal(rootState)).toEqual(story);
+  });
+});
