e40d2aac688f7e6e5a92733b4a81bfb1e0348686
snamiki1212 <snamiki1212@gmail.com>
2021-07-18 22:28:42 -0700

    Refactor test code

--- src/redux/ui/eventModal.test.ts
+++ src/redux/ui/eventModal.test.ts
@@ -7,66 +7,81 @@ import reducer, {
 } from "./eventModal";
 import { RootState } from "../rootReducer";
 
-const createRootState = (partialState: any) =>
-  ({
-    ui: { eventModal: partialState },
-  } as RootState);
+type DummyCalendar = any;
+type DummyStory = any;
+type DummyEvent = any;
 
-const initialState = { event: null };
-
-const createDummyEvent = () => ({
+const createDummyCalendar = ({ id }: { id: any }) => ({ id } as DummyCalendar);
+const createDummyStory = ({ id }: { id: any }) => ({ id } as DummyStory);
+const createDummyEvent = ({ id }: { id: String }) => ({ id } as DummyEvent);
+const createDummyEventModal = () => ({
   calendarId: "calendarId",
   storyId: "storyId",
   eventId: "eventId",
 });
 
+type DummyEventModal = ReturnType<typeof createDummyEventModal>;
+
+const createRootState = ({
+  eventModalInfo = null,
+  calendars = [],
+}: {
+  eventModalInfo?: DummyEventModal;
+  calendars?: DummyCalendar[];
+}) =>
+  ({
+    ui: { eventModal: { event: eventModalInfo } },
+    features: { userCalendars: { calendars } },
+  } as any as RootState);
+
 describe(reducer.name, () => {
   it("can save init state", () => {
+    const initialState = { event: null };
     expect(reducer(undefined, {} as any)).toEqual(initialState);
   });
 
   describe(pushAction.name, () => {
     it("can work when not to have prev state.", () => {
-      const prevState = { event: null };
-      const event = createDummyEvent();
-      expect(reducer(prevState, pushAction(event))).toEqual({
-        event,
-      });
+      const modalInfo = createDummyEventModal();
+      const befState = { event: null };
+      const aftState = { event: modalInfo };
+      expect(reducer(befState, pushAction(modalInfo))).toEqual(aftState);
     });
   });
 
   describe(popAction.name, () => {
     it("can work when to have prev state.", () => {
-      const event = createDummyEvent();
-      const prevState = { event };
-      expect(reducer(prevState, popAction())).toEqual({ event: null });
+      const modalInfo = createDummyEventModal();
+      const befState = { event: modalInfo };
+      const aftState = { event: null };
+      expect(reducer(befState, popAction())).toEqual(aftState);
     });
   });
 });
 
 describe(selectIsOpen.name, () => {
   it("should be false when to close.", () => {
-    const rootState = createRootState({ event: null });
+    const rootState = createRootState({ eventModalInfo: null });
     expect(selectIsOpen(rootState)).toBe(false);
   });
 
   it("should be true when to open.", () => {
-    const event = createDummyEvent();
-    const rootState = createRootState({ event });
+    const modal = createDummyEventModal();
+    const rootState = createRootState({ eventModalInfo: modal });
     expect(selectIsOpen(rootState)).toBe(true);
   });
 });
 
 describe(selectEventModal.name, () => {
   it("should exist when to open.", () => {
-    const event = createDummyEvent();
-    const rootState = createRootState({ event });
-    expect(selectEventModal(rootState)).toEqual(event);
+    const modal = createDummyEventModal();
+    const rootState = createRootState({ eventModalInfo: modal });
+    expect(selectEventModal(rootState)).toEqual(modal);
   });
 
   it("should not exist when to close.", () => {
-    const event = null;
-    const rootState = createRootState({ event });
+    const modal = null;
+    const rootState = createRootState({ eventModalInfo: modal });
     expect(selectEventModal(rootState)).toEqual(null);
   });
 });
