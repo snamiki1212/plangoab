79aee763d5c15e64f8d73f44bb582dd3a98d2f77
snamiki1212 <snamiki1212@gmail.com>
2021-07-19 20:04:55 -0700

    Refactor selector logic

--- src/redux/ui/storyModal.ts
+++ src/redux/ui/storyModal.ts
@@ -1,5 +1,6 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction, createSelector } from "@reduxjs/toolkit";
 import { RootState } from "@/redux/rootReducer";
+import { selectStoryByIdFilter } from "@/redux/features/userCalendars";
 
 type State = {
   story: null | {
@@ -35,28 +36,14 @@ export const selectIsOpen = (state: RootState) => !!state.ui.storyModal.story;
 
 export const selectStoryModal = (state: RootState) => state.ui.storyModal.story;
 
-export const selectStory = (state: RootState) => {
-  const _story = state.ui.storyModal.story;
-  if (!_story) return null;
-  const { calendarId, storyId } = _story;
-
-  // calendar
-  const calendarIdx = state.features.userCalendars.calendars.findIndex(
-    (calendar) => calendar.id === calendarId
-  );
-  const cannotFind = calendarIdx === -1;
-  if (cannotFind) {
-    console.warn(`Cannot find calendar: ${calendarId}.`);
-    return;
+export const selectStory = createSelector(
+  [selectStoryModal, selectStoryByIdFilter],
+  (modal, filter) => {
+    if (!modal) return null;
+    const { calendarId, storyId } = modal;
+    return filter(calendarId, storyId);
   }
-
-  // story
-  const story = state.features.userCalendars.calendars[
-    calendarIdx
-  ].stories.find((story) => story.id === storyId);
-  if (!story) return console.warn(`Cannot find story: ${storyId}.`);
-  return story;
-};
+);
 
 export const { push: pushAction, pop: popAction } = slice.actions;
 
