815a40461d049de8d368b9b9a2ec1ed016f20871
snamiki1212 <snamiki1212@gmail.com>
2021-07-21 23:44:46 -0700

    Refactor test of user reducer

--- src/redux/features/user.test.ts
+++ src/redux/features/user.test.ts
@@ -8,67 +8,64 @@ const getBirthday = (today: Date) =>
 
 describe("[mock before import files because using date at global scope]", () => {
   const mockDate = new Date(2005, 1, 1);
-
+  let reducer, selectUserWithAge, updateBirthdayAction, resetAction;
   beforeAll(() => {
+    // mock date
     jest.useFakeTimers("modern");
     jest.setSystemTime(mockDate);
+
+    // lazy load
+    const modules = require("./user");
+    reducer = modules.default;
+    selectUserWithAge = modules.selectUserWithAge;
+    updateBirthdayAction = modules.updateBirthdayAction;
+    resetAction = modules.resetAction;
   });
 
   afterAll(() => {
     jest.clearAllMocks();
   });
 
-  describe("[lazy load]", () => {
-    let reducer, selectUserWithAge, updateBirthdayAction, resetAction;
-    beforeAll(() => {
-      const modules = require("./user");
-      reducer = modules.default;
-      selectUserWithAge = modules.selectUserWithAge;
-      updateBirthdayAction = modules.updateBirthdayAction;
-      resetAction = modules.resetAction;
+  describe(toStr({ reducer }), () => {
+    it("can init.", () => {
+      const initialState = {
+        birthday: getBirthday(mockDate),
+      };
+      expect(reducer(undefined, {} as any)).toEqual(initialState);
     });
+  });
 
-    describe(toStr({ reducer }), () => {
-      it("can init.", () => {
-        const initialState = {
-          birthday: getBirthday(mockDate),
-        };
-        expect(reducer(undefined, {} as any)).toEqual(initialState);
-      });
+  describe(toStr({ updateBirthdayAction }), () => {
+    it("can work.", () => {
+      const befBirthday = new Date(2999, 1, 1);
+      const aftBirthday = new Date();
+      const befState = { birthday: befBirthday.toISOString() };
+      const aftState = { birthday: aftBirthday.toISOString() };
+      expect(
+        reducer(
+          befState,
+          updateBirthdayAction({ birthday: aftBirthday.toISOString() })
+        )
+      ).toEqual(aftState);
     });
+  });
 
-    describe(toStr({ updateBirthdayAction }), () => {
-      it("can work.", () => {
-        const today = new Date(2999, 1, 1);
-
-        const birthday = new Date();
-        const befState = { birthday: today.toISOString() };
-        const aftState = { birthday: birthday.toISOString() };
-        expect(
-          reducer(
-            befState,
-            updateBirthdayAction({ birthday: birthday.toISOString() })
-          )
-        ).toEqual(aftState);
-      });
+  describe(toStr({ resetAction }), () => {
+    it("can work.", () => {
+      const befState = { birthday: getBirthday(new Date(1990, 1, 1)) };
+      const aftState = { birthday: getBirthday(mockDate) };
+      expect(reducer(befState, resetAction())).toEqual(aftState);
     });
-    describe(toStr({ resetAction }), () => {
-      it("can work.", () => {
-        const befState = { birthday: getBirthday(new Date(1990, 1, 1)) };
-        const aftState = { birthday: getBirthday(mockDate) };
-        expect(reducer(befState, resetAction())).toEqual(aftState);
-      });
-    });
+  });
 
-    describe(toStr({ selectUserWithAge }), () => {
-      const createRootState = (partialState: any) =>
-        ({ features: { user: partialState } } as RootState);
+  describe(toStr({ selectUserWithAge }), () => {
+    const createRootState = (partialState: any) =>
+      ({ features: { user: partialState } } as RootState);
 
-      it("can select.", () => {
-        const dummyUser = { birthday: "2000-01-01", age: 5 };
-        const rootState = createRootState(dummyUser);
-        expect(selectUserWithAge(rootState)).toEqual(dummyUser);
-      });
+    it("can select.", () => {
+      const dummyUser = { birthday: "2000-01-01", age: 5 }; // 2005 - 5 = 2000
+      const rootState = createRootState(dummyUser);
+      expect(selectUserWithAge(rootState)).toEqual(dummyUser);
     });
   });
 });
