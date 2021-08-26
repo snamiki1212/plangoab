49a971fb560fb78e80908f63668fc1e2862aa69f
snamiki1212 <snamiki1212@gmail.com>
2021-08-26 12:40:31 -0700

    Update LP hero section

--- src/components/molecules/LpHeroSection.tsx
+++ src/components/molecules/LpHeroSection.tsx
@@ -1,11 +1,42 @@
+import styled from "styled-components";
 import Image from "next/image";
+import Grid from "@material-ui/core/Grid";
+import Container from "@material-ui/core/Container";
+import Button from "@material-ui/core/Button";
 
 export function LpHeroSection() {
   return (
-    <div>
-      <div>this is hero</div>
-      <Image src="/assets/airplane.png" width={680} height={382} />
-      <div>this is hero</div>
-    </div>
+    <Container maxWidth="lg">
+      <Grid container>
+        <Grid
+          item
+          md={6}
+          alignItems="center"
+          justify="center"
+          style={{ display: "flex", flexDirection: "column" }}
+        >
+          <AppName>Plangoab</AppName>
+          <Text>Make a success your planning</Text>
+          <Button variant="contained" color="primary">
+            Start Now
+          </Button>
+        </Grid>
+        <Grid item md={6}>
+          <Image src="/assets/airplane.png" width={680} height={382} />
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
+
+const AppName = styled.div`
+  color: var(--logo);
+  font-family: var(--font-design1);
+`;
+
+const Text = styled.div`
+  color: var(--base-dark1);
+  font-family: var(--font-text1);
+
+  margin-bottom: 2rem;
+`;
