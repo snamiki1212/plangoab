79d0efde07c3484a7fe1d5a54d4d4f1c618e02f4
snamiki1212 <snamiki1212@gmail.com>
2021-08-26 12:49:54 -0700

    Update LP Demo section

--- src/components/molecules/LpDemoSection.tsx
+++ src/components/molecules/LpDemoSection.tsx
@@ -1,3 +1,40 @@
+import styled from "styled-components";
+import Image from "next/image";
+import Grid from "@material-ui/core/Grid";
+import Container from "@material-ui/core/Container";
+
 export function LpDemoSection() {
-  return <div>this is demo</div>;
+  return (
+    <Wrapper>
+      <Container maxWidth="lg">
+        <Grid container>
+          <Grid
+            item
+            md={6}
+            alignItems="center"
+            justify="center"
+            style={{ display: "flex", flexDirection: "column" }}
+          >
+            <Title>Create your plan</Title>
+            <li>
+              <Text>Use template</Text>
+            </li>
+            <li>
+              <Text>Save your plan</Text>
+            </li>
+          </Grid>
+          <Grid item md={6}>
+            <Image src="/assets/lp_calendar.png" width={680} height={382} />
+          </Grid>
+        </Grid>
+      </Container>
+    </Wrapper>
+  );
 }
+
+const Title = styled.div``;
+const Text = styled.span``;
+
+const Wrapper = styled.div`
+  background: lightyellow;
+`;
