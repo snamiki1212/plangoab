1e70483d12d9de2bc6ef1e07e724a0a77d608fb5
snamiki1212 <snamiki1212@gmail.com>
2021-08-26 13:23:08 -0700

    Update LP footer

--- src/components/molecules/WebFooter.tsx
+++ src/components/molecules/WebFooter.tsx
@@ -1,11 +1,58 @@
-import Container from "@material-ui/core/Container";
 import styled from "styled-components";
+import Container from "@material-ui/core/Container";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import { SNS_LIST } from "@/constants/sns";
+
+const SNS_DESCRIPTIONS = SNS_LIST.map((sns) => ({
+  title: sns.name,
+  url: sns.url,
+}));
+
+const footers = [
+  {
+    title: "Team",
+    description: [
+      { title: "Home", url: "#" },
+      { title: "Create Calendar", url: "#" },
+      { title: "Contact us", url: "#" },
+    ],
+  },
+  {
+    title: "Featuers",
+    description: [
+      { title: "Create Calendar", url: "#" },
+      { title: "Show created calendars", url: "#" },
+    ],
+  },
+  { title: "Creater", description: [...SNS_DESCRIPTIONS] },
+];
 
 export function WebFooter() {
-  return <_Container>this is footer</_Container>;
+  return (
+    <Wrapper>
+      <Container maxWidth="md">
+        <Grid container spacing={4} justify="space-evenly">
+          {footers.map((footer) => (
+            <Grid item xs={6} sm={3} key={footer.title}>
+              <span>{footer.title}</span>
+              <ul>
+                {footer.description.map(({ title, url }) => (
+                  <li key={title}>
+                    <a href={url}>{title}</a>
+                  </li>
+                ))}
+              </ul>
+            </Grid>
+          ))}
+        </Grid>
+        <Box mt={5}>Copyright Shun Namiki</Box>
+      </Container>
+    </Wrapper>
+  );
 }
 
-const _Container = styled(Container)`
-  min-height: 15rem;
-  background: lightblue;
+const Wrapper = styled.div`
+  padding-top: 10rem;
+  background: lightpink;
 `;
