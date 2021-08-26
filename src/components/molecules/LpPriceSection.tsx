e2118a44b6e5eb139894eb29b49cc7d22b7562a9
snamiki1212 <snamiki1212@gmail.com>
2021-08-26 11:38:31 -0700

    Add price section on home

---
+++ src/components/molecules/LpPriceSection.tsx
@@ -0,0 +1,139 @@
+import React from "react";
+import styled from "styled-components";
+import Button from "@material-ui/core/Button";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardHeader from "@material-ui/core/CardHeader";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
+
+const ENTERPRISE = "Enterprise";
+const FREE = "Free";
+
+const tiers = [
+  {
+    title: "Free",
+    subheader: "for indivisual",
+    price: FREE,
+    description: ["Create calendars", "Use templates"],
+    buttonVariant: "outlined" as const, // "text" | "outlined" | "contained"
+    buttonText: "Start Now",
+  },
+  {
+    title: ENTERPRISE,
+    subheader: "for company",
+    price: "100",
+    description: ["Create calendars", "Use templates", "Save calendars"],
+    buttonVariant: "contained" as const, // "text" | "outlined" | "contained"
+    buttonText: "Start trial",
+  },
+];
+
+const HeroUnit = () => {
+  return (
+    <Container
+      maxWidth="sm"
+      component="main"
+      // className={classes.heroContent}
+    >
+      <Typography
+        component="h1"
+        variant="h2"
+        align="center"
+        color="textPrimary"
+        gutterBottom
+      >
+        Pricing
+      </Typography>
+      <Typography
+        variant="h5"
+        align="center"
+        color="textSecondary"
+        component="p"
+      >
+        Quickly build an effective pricing table for your potential customers
+        with this layout. It&apos;s built with default Material-UI components
+        with little customization.
+      </Typography>
+    </Container>
+  );
+};
+
+const PriceUnit = () => {
+  return (
+    <Container maxWidth="md" component="main">
+      <Grid container spacing={5} alignItems="flex-end">
+        {tiers.map((tier) => (
+          <Grid
+            item
+            key={tier.title}
+            xs={12}
+            sm={tier.title === ENTERPRISE ? 7 : 5}
+            md={tier.title === ENTERPRISE ? 7 : 5}
+          >
+            <Card>
+              <CardHeader
+                title={tier.title}
+                subheader={tier.subheader}
+                titleTypographyProps={{ align: "center" }}
+                subheaderTypographyProps={{ align: "center" }}
+                action={tier.title === ENTERPRISE ? <span>⭐️</span> : null}
+                // className={classes.cardHeader}
+              />
+              <CardContent>
+                <PriceContainer>
+                  <Typography component="h2" variant="h3" color="textPrimary">
+                    {tier.price === FREE ? FREE : `\$${tier.price}`}
+                  </Typography>
+                  <Typography variant="h6" color="textSecondary">
+                    /mo
+                  </Typography>
+                </PriceContainer>
+                <ul>
+                  {tier.description.map((line) => (
+                    <Typography
+                      component="li"
+                      variant="subtitle1"
+                      align="center"
+                      key={line}
+                    >
+                      {line}
+                    </Typography>
+                  ))}
+                </ul>
+              </CardContent>
+              <CardActions>
+                <Button fullWidth variant={tier.buttonVariant} color="primary">
+                  {tier.buttonText}
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
+  );
+};
+
+export const LpPriceSection = () => {
+  return (
+    <Wrapper>
+      <HeroUnit />
+      <PriceUnit />
+    </Wrapper>
+  );
+};
+
+const PriceContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: baseline;
+  margin-bottom: 1rem;
+`;
+
+const Wrapper = styled.div`
+  background: lightgray;
+  padding: 3rem 0;
+`;
--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -3,6 +3,7 @@ import { WebFooter } from "@/components/molecules/WebFooter";
 import { LpDemoSection } from "@/components/molecules/LpDemoSection";
 import { LpSponseredSection } from "@/components/molecules/LpSponseredSection";
 import { LpHeroSection } from "@/components/molecules/LpHeroSection";
+import { LpPriceSection } from "@/components/molecules/LpPriceSection";
 
 export function HomePage() {
   return (
@@ -11,6 +12,7 @@ export function HomePage() {
       <LpHeroSection />
       <LpDemoSection />
       <LpSponseredSection />
+      <LpPriceSection />
       <WebFooter />
     </div>
   );
