401496eef2245440e77365af4d88411ef3efc052
snamiki1212 <snamiki1212@gmail.com>
2021-08-26 18:55:43 -0700

    Fix path name

--- src/constants/collaborations.ts
+++ src/constants/collaborations.ts
@@ -2,7 +2,7 @@ export const collaborations = [
   {
     name: "Frog",
     link: "https://frogagent.com/",
-    logo: "/assets/collaborations_FROG.png",
+    logo: "/assets/collaborations_Frog.png",
   },
   {
     name: "COS",
