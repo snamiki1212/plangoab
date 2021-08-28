80f63a566f4f9a549fc4701672e4965bf83bdc68
snamiki1212 <snamiki1212@gmail.com>
2021-08-27 23:17:30 -0700

    Add demo section on LP

---
+++ src/components/molecules/LpDemoSection.tsx
@@ -0,0 +1,79 @@
+import Link from "next/link";
+import styled from "styled-components";
+import Grid from "@material-ui/core/Grid";
+import Container from "@material-ui/core/Container";
+import { ROUTES } from "@/constants/routes";
+
+export function LpDemoSection() {
+  return (
+    <Wrapper>
+      <Container maxWidth="lg">
+        <Grid container>
+          <Grid item md={6} sm={12}>
+            <img
+              src="/assets/demo_edit_calendar.gif"
+              width={680}
+              height={382}
+              style={{ margin: "0 auto", objectFit: "scale-down" }}
+            />
+          </Grid>
+          <Grid
+            item
+            md={6}
+            sm={12}
+            alignItems="center"
+            justify="center"
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Title>Demo</Title>
+            <TextContainer>
+              <Text>Create your plan!</Text>
+              <Text>
+                <Link href={ROUTES.CALENDARS__NEW}>
+                  <a style={{ textDecoration: "none" }}>👉 Start Plangoab</a>
+                </Link>
+              </Text>
+            </TextContainer>
+          </Grid>
+        </Grid>
+      </Container>
+    </Wrapper>
+  );
+}
+
+const Title = styled.div`
+  font-family: var(--font-header1);
+  font-size: 2.5rem;
+  color: var(--color-dark1);
+  font-weight: 900;
+  text-align: center;
+  /*  */
+  margin-bottom: 2rem;
+`;
+
+const Text = styled.span`
+  font-family: var(--font-text1);
+  font-size: 1.3rem;
+  color: var(--color-dark1);
+  /*  */
+  margin-left: 1rem;
+`;
+
+const TextContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1.5rem;
+`;
+
+const Wrapper = styled.div`
+  background: linear-gradient(
+    135deg,
+    var(--color-lp-bg1),
+    var(--color-light1) 30%
+  );
+  padding: 10rem 0;
+`;
--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -5,6 +5,7 @@ import { LpCollaborationsSection } from "@/components/molecules/LpCollaborations
 import { LpHeroSection } from "@/components/molecules/LpHeroSection";
 import { LpPriceSection } from "@/components/molecules/LpPriceSection";
 import { LpExplanationSection } from "@/components/molecules/LpExplanationSection";
+import { LpDemoSection } from "@/components/molecules/LpDemoSection";
 
 export function HomePage() {
   return (
@@ -12,6 +13,7 @@ export function HomePage() {
       <WebHeader />
       <LpHeroSection />
       <LpExplanationSection />
+      <LpDemoSection />
       <LpFeatureSection />
       <LpPriceSection />
       <LpCollaborationsSection />
