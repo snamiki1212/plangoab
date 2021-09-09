3c12327a20b347e49aebbfcdf6865f0cc6548247
snamiki1212 <snamiki1212@gmail.com>
2021-09-08 22:23:36 -0700

    Remove empty-calendar component

--- src/components/templates/CalendarPageContent.tsx
+++ src/components/templates/CalendarPageContent.tsx
@@ -1,27 +1,17 @@
-import React from "react";
 import styled from "styled-components";
 import { UserCalendarSection } from "./UserCalendarSection";
 import { CollegeCalendarSection } from "./CollegeCalendarSection";
-import { EmptyCalendar } from "@/components/molecules/EmptyCalendar";
-import { useUserCalendar } from "@/hooks/useUserCalendar";
 
 export function CalendarPageContent() {
-  const { isAlreadyCreated: shouldRenderCalendarSection } = useUserCalendar();
   return (
-    <div>
-      {shouldRenderCalendarSection ? (
-        <InnerContainer>
-          <UserCalendarSection />
-          <CollegeCalendarSection />
-        </InnerContainer>
-      ) : (
-        <EmptyCalendar />
-      )}
-    </div>
+    <Container>
+      <UserCalendarSection />
+      <CollegeCalendarSection />
+    </Container>
   );
 }
 
-const InnerContainer = styled.div`
+const Container = styled.div`
   display: grid;
   gap: 3rem;
 `;
