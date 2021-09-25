63587f918e30fc579cf19d098823832020804638
snamiki1212 <snamiki1212@gmail.com>
2021-09-25 10:51:10 -0700

    Update error/loading page

--- src/components/pages/CalendarsListPage.tsx
+++ src/components/pages/CalendarsListPage.tsx
@@ -3,13 +3,15 @@ import { CalendarListContent } from "@/components/templates/CalendarListContent"
 import { Footer } from "@/components/molecules/Footer";
 import { AppHeader } from "@/components/molecules/AppHeader";
 import { useGetCalendarsQuery } from "@/redux/services/calendarApiV1";
+import { ErrorPage } from "@/components/pages/ErrorPage";
+import { LoadingPage } from "@/components/pages/LoadingPage";
 //
 
 export const CalendarsListPage = () => {
   const { data, error, isLoading } = useGetCalendarsQuery("");
 
-  if (error) return <div>Error</div>;
-  if (isLoading) return <div>loading...</div>;
+  if (error) return <ErrorPage />;
+  if (isLoading) return <LoadingPage />;
 
   console.log({ data });
   return (
