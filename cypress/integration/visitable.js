095dad491669efad767447bec0176b422c32e3c1
snamiki1212 <snamiki1212@gmail.com>
2021-10-03 00:59:26 -0700

    Add e2e test

--- cypress/integration/visitable.js
+++ cypress/integration/visitable.js
@@ -3,15 +3,19 @@ describe("All pages are visitable", () => {
     cy.visit("/home");
   });
 
-  // it("calendars list page.", () => {
-  //   cy.visit("/calendars");
-  // });
-
   it("calendars new page.", () => {
     cy.visit("/");
   });
 
-  // it("calendars detail page.", () => {
-  //   cy.visit("/calendars/1");
-  // });
+  it("calendars list page.", () => {
+    cy.visit("/calendars");
+  });
+
+  it("calendars create page.", () => {
+    cy.visit("/calendars/create");
+  });
+
+  it("calendars detail page.", () => {
+    cy.visit("/calendars/1");
+  });
 });
