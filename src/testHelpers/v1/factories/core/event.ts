17275b7fd64f6085e36a836a45c620e2632b21b0
snamiki1212 <snamiki1212@gmail.com>
2021-10-02 18:06:44 -0700

    Update test helper' test

--- src/testHelpers/v1/factories/core/event.ts
+++ src/testHelpers/v1/factories/core/event.ts
@@ -5,9 +5,19 @@ const decorateId = (id: number) => `EVENT_${id}`;
 
 export const eventFactory = Factory.define<BaseEvent>(({ sequence }) => ({
   id: decorateId(sequence),
+  calendarId: "calendarId",
+  resourceId: "resourceId",
+  storyId: "storyId",
+  title: "event title",
+  start: "1990-01-01 00:00:00",
+  end: "1990-01-01 00:00:00",
+  description: "description-title",
+  //
   extendedProps: {
     calendarId: "calendarId",
     storyId: "storyId",
+    resourceId: "resourceId",
     description: "description",
   },
+  backgroundColor: "red",
 }));
