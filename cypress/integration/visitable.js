7158a8c84bc8d352ff3c89e4455a6062fee93ea0
snamiki1212 <snamiki1212@gmail.com>
2021-10-21 13:54:31 -0700

    feat: update test

--- cypress/integration/visitable.js
+++ cypress/integration/visitable.js
@@ -1,18 +1,18 @@
 describe("All pages are visitable", () => {
   it("top page.", () => {
-    cy.visit("/home");
+    cy.visit("/");
   });
 
-  it("calendars new page.", () => {
-    cy.visit("/");
+  it("calendars create page.", () => {
+    cy.visit("/calendars/create");
   });
 
   it("calendars list page.", () => {
     cy.visit("/calendars");
   });
 
-  it("calendars create page.", () => {
-    cy.visit("/calendars/create");
+  it("calendars create page V1.", () => {
+    cy.visit("/calendars/createV1");
   });
 
   it("calendars detail page.", () => {
